cae14a5e5425ac717669795078e9829f8a628e19
Utsav Suhagiya <ustavpatel2004@gmail.com>
2024-04-10 08:20:16 +0530

    React-Main

---
+++ LECTURE-2/myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from './Components/Heading';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading/>
+    </div>
+  );
+}
+
+export default App;
